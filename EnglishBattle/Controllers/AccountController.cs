ad8d0205376e9110b474ac03638da75a992e86be
rbary <rbary.lite@gmail.com>
2021-03-03 08:56:14 +0100

    Adding subscription view

---
+++ EnglishBattle/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EnglishBattle.Data;
+using EnglishBattle.Data.Services;
+
+namespace EnglishBattle.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Subscription
+        public ActionResult Subscription()
+        {
+            return View();
+        }
+
+        /*[HttpGet]
+        public ActionResult GetVilles(string id)
+        {
+            if (!string.IsNullOrWhiteSpace(iso3) && iso3.Length == 3)
+            {
+                var repo = new RegionsRepository();
+
+                IEnumerable<SelectListItem> regions = repo.GetRegions(iso3);
+                return Json(regions, JsonRequestBehavior.AllowGet);
+            }
+            return null;
+        }*/
+
+        // POST: Subscription
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Subscription (Models.SubscriptionViewModel model)
+        {
+            // Validation coté serveur
+            if (ModelState.IsValid)
+            {
+                // inscription en base données d'un joueur
+                JoueurService joueurService = new JoueurService(new EnglishBattleEntities());
+
+                Joueur joueur = new Joueur();
+
+                joueur.nom = model.Nom;
+                joueur.prenom = model.Prenom;
+                joueur.email = model.Email;
+                joueur.motDePasse = model.MotDePasse;
+                joueur.niveau = model.Niveau;
+                joueur.idVille = model.IdVilleSelectionnee;
+
+                joueurService.CreateJoueur(joueur);
+
+                // rediriger sur la page d'acceuil
+                return RedirectToAction("Index", "Home");
+            }
+
+            // Si on arrive ici c'est que les données du formulaire n'est pas validé
+            return View();
+        }
+    }
+}
---
+++ EnglishBattle/Models/SubscriptionViewModel.cs
@@ -0,0 +1,44 @@
+﻿using EnglishBattle.Data;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace EnglishBattle.Models
+{
+    public class SubscriptionViewModel
+    {
+        [Required] // indique que ce champ est obligatoire
+        [Display(Name = "Nom")]
+        public string Nom { get; set; }
+
+        [Required]
+        [Display(Name = "Prénom")] // texte utiliser dans le label associé au champ
+        public string Prenom { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress] // valide si l'email est valide
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Mot de passe")]
+        [DataType(DataType.Password)] // indique que ce champ sera de type password
+        [StringLength(14, ErrorMessage = "Le mot de passe doit compris entre {2} et {1}", MinimumLength = 6)]
+        public string MotDePasse { get; set; }
+
+        [Required]
+        [Display(Name = "Confirmer le mot de passe")]
+        [DataType(DataType.Password)]
+        [System.ComponentModel.DataAnnotations.Compare("MotDePasse", ErrorMessage = "Le mot de passe et le mot de passe de confirmation ne correspondent pas.")]
+        public string ConfirmationMotDePasse { get; set; }
+
+        [Required]
+        [Display(Name = "Niveau")]
+        public int Niveau { get; set; }
+
+        [Required]
+        [Display(Name = "Ville")]
+        public int IdVilleSelectionnee { get; set; }
+        public IEnumerable<SelectListItem> Villes { get; set; }
+    }
+}
